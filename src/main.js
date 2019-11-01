56ea4d7b51c8487a23cd86d9871c035fffe40b5d
liulingyu <liulingyu@lechebang.com>
2019-11-01 16:46:42 +0800

    feat(loading.js):全局混入loading服务

--- src/main.js
+++ src/main.js
@@ -2,17 +2,30 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store';
+import './permission';
 import iView from 'iview'
 import {
   filter
 } from './shared/filter';
 import 'iview/dist/styles/iview.css';
+import {
+  showLoading,
+  hideLoading
+} from './shared/loading';
 
 Vue.config.productionTip = false
 Vue.use(iView);
 
 Object.keys(filter).forEach(key => {
   Vue.filter(key, filter[key]);
+});
+
+// 全局混入
+Vue.mixin({
+  methods: {
+    showLoading,
+    hideLoading
+  }
 });
 
 new Vue({
