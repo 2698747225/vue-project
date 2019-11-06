6ec29a9d7267eacac0179096905b508077bba335
liulingyu <liulingyu@lechebang.com>
2019-11-06 17:26:33 +0800

    feat(*):添加工具包中类型验证

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -6,3 +6,38 @@ export function validEmail(email) {
     const reg = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/
     return reg.test(email)
 }
+
+/**
+ * @param {string} mobile
+ * @returns {Boolean}
+ */
+export function validMobile(mobile) {
+    const reg = /^1[1-9][0-9]{9}$/
+    return reg.test(mobile);
+}
+
+/**
+ * @param {Array} array
+ * @returns {Boolean}
+ */
+export function isArray(array) {
+    return Object.prototype.toString.call(array) === '[object Array]';
+}
+
+/**
+ * @param {Object} obj
+ * @returns {Boolean}
+ */
+export function isObject(obj) {
+    // 函数、对象、数组、并且不为null
+    const type = typeof obj;
+    return type === 'object' && !!obj;
+}
+
+/**
+ * @param {Object} obj
+ * @returns {Boolean}
+ */
+export function isUndefined(obj) {
+    return obj === void 0;
+}
