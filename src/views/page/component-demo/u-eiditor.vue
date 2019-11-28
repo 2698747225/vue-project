61d33f80dfd9692bb569cf0daf549f015ffdaa04
2698747225 <2698747225@qq.com>
2019-11-28 16:07:14 +0800

    feat(*):一部分未完成的代码提交

---
+++ src/views/page/component-demo/u-eiditor.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Card class="ueditor-desc" :bordered="false">
+      <label>富文本是管理后端的一个核心特性，但同时它也是一个有很多凹坑的地方。在选择丰富文本的过程中，我也走了很多弯路。市场上常见的丰富文本基本上都用上了，我最后选择了tinymce。请参阅更详细的富文本比较和介绍</label>
+      <a>查看</a>
+    </Card>
+    <nz-card></nz-card>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="less" scoped>
+.ueditor-desc {
+  background-color: #eef1f6;
+}
+</style>
---
+++ src/views/page/icon-demo/icons.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <Card :bordered="false">link</Card>
+    <Card>
+      <Scroll :on-reach-bottom="handleReachBottom">
+        <Card
+          dis-hover
+          v-for="(item, index) in list1"
+          :key="index"
+          style="margin: 32px 0"
+        >Content {{ item }}</Card>
+      </Scroll>
+    </Card>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    handleReachBottom() {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          const last = this.list1[this.list1.length - 1];
+          for (let i = 1; i < 11; i++) {
+            this.list1.push(last + i);
+          }
+          resolve();
+        }, 2000);
+      });
+    }
+  }
+};
+</script>
+<style lang="less" scoped>
+</style>
